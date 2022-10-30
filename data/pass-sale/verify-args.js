03c4f209e2c007103c36b7f03c00ef8e0f8bc2fd
Roman Bobrik <bobrom27@gmail.com>
2022-10-30 13:41:23 +0300

    Deploy contracts to Goerli

--- data/pass-sale/verify-args.js
+++ data/pass-sale/verify-args.js
@@ -1,8 +1,8 @@
 module.exports = [
-  "0x6A9026FF7a018f79FB1209602574C4876428bA40",
+  "0x9fA146CE786059e6F7960A3a65588d6b030D1763",
   "0x052aA161FabaAF145a3356733591BE0AC92560De",
   "0x9c2BcCC4cad05B2ca83243c109613aC78342bfF3",
   "ipfs://bafkreic7g3c57uef4sw7yxn7exx6eeugv4ynuoxle5yalorxkzqw5kz7xq",
   "ipfs://bafybeicajjv7xymvm57xygq35edjagq7x2lq7giwg67wvdb3klscrunyve",
-  "5000000000000000",
+  "500000000000000000",
 ];
