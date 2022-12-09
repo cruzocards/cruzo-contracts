b4afb98d0c24ddccbd5c563fe78669a7dc498b1c
Nikita Korshunov <kewka96@gmail.com>
2022-12-09 18:34:17 +0400

    feat(airdrop): CruzoAirdrop contract

---
+++ contracts/airdrop/CruzoAirdrop.sol
@@ -0,0 +1,100 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/utils/ERC1155HolderUpgradeable.sol";
+
+import "../transfer-proxy/ITransferProxy.sol";
+
+contract CruzoAirdrop is
+    Initializable,
+    UUPSUpgradeable,
+    OwnableUpgradeable,
+    ERC1155HolderUpgradeable
+{
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+
+    struct Drop {
+        address tokenAddress;
+        uint256 tokenId;
+        uint256 amount;
+        uint256 claimed;
+        mapping(address => bool) claimers;
+    }
+
+    event AirdropCreated(
+        uint256 id,
+        address tokenAddress,
+        uint256 tokenId,
+        address creator,
+        uint256 amount
+    );
+
+    event AirdropClaimed(uint256 id, address claimer);
+
+    CountersUpgradeable.Counter private ids;
+
+    ITransferProxy public transferProxy;
+
+    mapping(uint256 => Drop) public drops;
+
+    constructor() {}
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+
+    function initialize(ITransferProxy _transferProxy) public initializer {
+        __Ownable_init();
+        transferProxy = _transferProxy;
+    }
+
+    function claim(uint256 _id) external {
+        Drop storage drop = drops[_id];
+        require(drop.amount > 0, "Airdrop: not found");
+        require(drop.claimed < drop.amount, "Airdrop: closed");
+        require(!drop.claimers[_msgSender()], "Airdrop: already claimed");
+        drop.claimed++;
+        drop.claimers[_msgSender()] = true;
+        transferProxy.safeTransferFrom(
+            IERC1155Upgradeable(drop.tokenAddress),
+            address(this),
+            _msgSender(),
+            drop.tokenId,
+            1,
+            ""
+        );
+        emit AirdropClaimed(_id, _msgSender());
+    }
+
+    function create(
+        address _tokenAddress,
+        uint256 _tokenId,
+        uint256 _amount
+    ) external onlyOwner {
+        require(_amount > 0, "Airdrop: amount must be greater than 0");
+        ids.increment();
+        Drop storage drop = drops[ids.current()];
+        drop.tokenAddress = _tokenAddress;
+        drop.tokenId = _tokenId;
+        drop.amount = _amount;
+        transferProxy.safeTransferFrom(
+            IERC1155Upgradeable(_tokenAddress),
+            _msgSender(),
+            address(this),
+            _tokenId,
+            _amount,
+            ""
+        );
+        emit AirdropCreated(
+            ids.current(),
+            _tokenAddress,
+            _tokenId,
+            _msgSender(),
+            _amount
+        );
+    }
+}
---
+++ test/CruzoAirdrop.ts
@@ -0,0 +1,202 @@
+import { expect } from "chai";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { ethers, upgrades } from "hardhat";
+import { CruzoAirdrop, TransferProxy } from "../typechain";
+
+describe("CruzoAirdrop", () => {
+  async function fixture() {
+    const signers = await ethers.getSigners();
+
+    const TransferProxy = await ethers.getContractFactory("TransferProxy");
+    const transferProxy = (await upgrades.deployProxy(TransferProxy, [], {
+      kind: "uups",
+    })) as TransferProxy;
+    await transferProxy.deployed();
+
+    const CruzoAirdrop = await ethers.getContractFactory("CruzoAirdrop");
+    const airdrop = (await upgrades.deployProxy(
+      CruzoAirdrop,
+      [transferProxy.address],
+      {
+        kind: "uups",
+      }
+    )) as CruzoAirdrop;
+    await airdrop.deployed();
+
+    await transferProxy.setOperator(airdrop.address, true);
+
+    const Cruzo1155 = await ethers.getContractFactory("Cruzo1155");
+    const token = await Cruzo1155.deploy();
+    await token.deployed();
+    await token.initialize(
+      "CRUZO",
+      "CRZ",
+      "baseURI",
+      "contractURI",
+      transferProxy.address,
+      true
+    );
+
+    return {
+      signers,
+      airdrop,
+      token,
+    };
+  }
+
+  it("Should deploy", async () => {
+    const { signers, airdrop } = await loadFixture(fixture);
+    expect(await airdrop.owner()).eq(signers[0].address);
+  });
+
+  describe("create", () => {
+    it("Should create a new airdrop", async () => {
+      const {
+        airdrop,
+        token,
+        signers: [creator, notowner],
+      } = await loadFixture(fixture);
+
+      const tokenId = 1;
+      const amount = 123;
+
+      await token
+        .connect(creator)
+        .create(tokenId, amount, creator.address, "", [], creator.address, 0);
+
+      await expect(
+        airdrop.connect(creator).create(token.address, tokenId, amount)
+      )
+        .emit(airdrop, "AirdropCreated")
+        .withArgs(1, token.address, tokenId, creator.address, amount);
+
+      const drop = await airdrop.drops(1);
+
+      expect(drop.tokenAddress).eq(token.address);
+      expect(drop.tokenId).eq(tokenId);
+      expect(drop.amount).eq(amount);
+      expect(drop.claimed).eq(0);
+
+      // onlyOwner
+      await expect(
+        airdrop.connect(notowner).create(token.address, tokenId, amount)
+      ).revertedWith("Ownable: caller is not the owner");
+    });
+
+    it("Should validate amount", async () => {
+      const {
+        signers: [creator],
+        token,
+        airdrop,
+      } = await loadFixture(fixture);
+      const tokenId = 1;
+      const amount = 0;
+
+      await expect(
+        airdrop.connect(creator).create(token.address, tokenId, amount)
+      ).revertedWith("Airdrop: amount must be greater than 0");
+    });
+
+    it("Should revert if the creator doesn't have enough tokens", async () => {
+      const {
+        signers: [creator],
+        token,
+        airdrop,
+      } = await loadFixture(fixture);
+      const tokenId = 1;
+      const amount = 123;
+      await expect(
+        airdrop.connect(creator).create(token.address, tokenId, amount)
+      ).revertedWith("ERC1155: insufficient balance for transfer");
+    });
+  });
+
+  describe("claim", () => {
+    it("Should claim", async () => {
+      const {
+        airdrop,
+        token,
+        signers: [creator, claimer],
+      } = await loadFixture(fixture);
+
+      const tokenId = 1;
+      const amount = 100;
+
+      await token
+        .connect(creator)
+        .create(tokenId, amount, creator.address, "", [], creator.address, 0);
+
+      await airdrop.connect(creator).create(token.address, tokenId, amount);
+
+      expect(await token.balanceOf(claimer.address, tokenId)).eq(0);
+
+      await expect(airdrop.connect(claimer).claim(1))
+        .emit(airdrop, "AirdropClaimed")
+        .withArgs(1, claimer.address);
+
+      expect(await token.balanceOf(claimer.address, tokenId)).eq(1);
+
+      const drop = await airdrop.drops(1);
+      expect(drop.claimed).eq(1);
+    });
+
+    it("Airdrop: not found", async () => {
+      const {
+        airdrop,
+        signers: [claimer],
+      } = await loadFixture(fixture);
+
+      await expect(airdrop.connect(claimer).claim(123)).revertedWith(
+        "Airdrop: not found"
+      );
+    });
+
+    it("Airdrop: closed", async () => {
+      const {
+        airdrop,
+        token,
+        signers: [creator, claimer1, claimer2, claimer3],
+      } = await loadFixture(fixture);
+
+      const tokenId = 1;
+      const amount = 2;
+
+      await token
+        .connect(creator)
+        .create(tokenId, amount, creator.address, "", [], creator.address, 0);
+
+      await airdrop.connect(creator).create(token.address, tokenId, amount);
+
+      await airdrop.connect(claimer1).claim(1);
+
+      await airdrop.connect(claimer2).claim(1);
+
+      await expect(airdrop.connect(claimer3).claim(1)).revertedWith(
+        "Airdrop: closed"
+      );
+    });
+
+    it("Airdrop: already claimed", async () => {
+      const {
+        airdrop,
+        token,
+        signers: [creator, claimer],
+      } = await loadFixture(fixture);
+
+      const tokenId = 1;
+      const amount = 100;
+
+      await token
+        .connect(creator)
+        .create(tokenId, amount, creator.address, "", [], creator.address, 0);
+
+      await airdrop.connect(creator).create(token.address, tokenId, amount);
+
+      await airdrop.connect(claimer).claim(1);
+
+      await expect(airdrop.connect(claimer).claim(1)).revertedWith(
+        "Airdrop: already claimed"
+      );
+    });
+  });
+});
